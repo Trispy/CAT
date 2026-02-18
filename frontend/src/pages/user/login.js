3908ddd5d2c6beaf9ca2bb975c7c0156abf5dcc5
Trispy <epyles@ufl.edu>
2026-02-17 22:45:55 -0500

    Work in progress

--- frontend/src/pages/user/login.js
+++ frontend/src/pages/user/login.js
@@ -1,5 +1,29 @@
+import './login.css';
+import Button from '../../components/button';
+import { Link } from 'react-router-dom';
+
 function Login() {
-  return <h1>Log in</h1>;
+  return <div className='form'>
+    <h1>Log In</h1>
+    <label class='input' type='text' name="username" placeholder="Your username..">
+    Username: <input name="username" />
+    </label>
+
+    <label class='input' type='text' name="email" placeholder="Your email..">
+    Email: <input name="email" />
+    </label>
+
+    <div className="home">
+        <Button text="Log In"></Button>
+    </div>
+
+    <div className="home">
+        <Link to="/createaccount">
+            <Button text="Create a New Account"></Button>
+        </Link>
+    </div>
+
+  </div>;
 }
 
 export { Login };
