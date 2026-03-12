1cb5239b70b655e5893e611ec9a033239befbf0c
Trispy <epyles@ufl.edu>
2026-03-12 13:25:19 -0400

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,11 @@ import { Home } from './pages/user/home.js';
 import { CreateAccount } from './pages/user/createAccount.js';
 import { Login } from './pages/user/login.js';
 import Symptoms from './pages/module1/symptoms.js'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PersonalHygiene from './pages/user/personalHygiene.js';
-import Cooking from './pages/module2/cooking.js';
+import PersonalHygiene from './pages/module1/personalHygiene.js';
+import Module2Part1 from './pages/module2/module2part1.js'
+import Location from './pages/module1/location.js'
+import Cleaning from './pages/module2/chopping.js';
+
 
 function App() {
   return (
@@ -16,8 +18,10 @@ function App() {
         <Route path="/createaccount" element={<CreateAccount />} />
         <Route path="/login" element={<Login />} />
         <Route path="/module1/symptoms" element={<Symptoms />} />
-        <Route path="/personalhygiene" element={<PersonalHygiene />} />
-        <Route path="/module2/cooking" element={<Cooking />} />
+        <Route path="/module1/personalHygiene" element={<PersonalHygiene />} />
+        <Route path="/module1/onLocation" element={<Location />} />
+        <Route path="/module2/therm" element={<Module2Part1/>} />
+        <Route path="/module2/cleaning" element={<Cleaning />} />
       </Routes>
     </BrowserRouter>
   );
