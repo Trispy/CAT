bbdd992c333219c9bb8cfbac161fef76dcd00dfa
arhaque09 <a.haque@ufl.edu>
2026-04-13 02:38:29 -0400

    timer not in weird place anymore

--- frontend/src/pages/module5/coldPreparedTransport.js
+++ frontend/src/pages/module5/coldPreparedTransport.js
@@ -465,8 +465,8 @@ export default function ColdPrepTransport({ openMenu }) {
                             this.timerStart = true;
                             let timeLeft = 10;
                             this.timerText = this.add.text(
-                                this.bg1.width * 1,
-                                this.bg1.height * 0.3,
+                                this.bg1.width * 0.81,
+                                this.bg1.height * 0.25,
                                 ":10",
                             {
                                 backgroundColor: "#000000",
