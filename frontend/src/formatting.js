63e195160102ab2664e4d871c6b1e77e93b46897
Corey <flores.bmichelle@ufl.eduCoreygit config --global user.name Coreygit pushlsgit config --global user.email flores.bmichelle@ufl.eduCorey>
2026-04-14 14:45:08 -0400

    m2g3 bug fixes

--- frontend/src/formatting.js
+++ frontend/src/formatting.js
@@ -1,4 +1,4 @@
 export const defaultFont='sans-serif';
 export const defaultFontSize='70px';
 export const defaultFontColor='#000';
-export const defaultTypingSpeed=10;
+export const defaultTypingSpeed=20;
