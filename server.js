9196aae717f2e7d62b93ff90fa6ee699d4949c1e
Trispy <epyles@ufl.edu>
2026-07-09 21:42:58 -0400

    Revert

--- server.js
+++ server.js
@@ -13,18 +13,6 @@ if (process.env.MONGODB_URI) {
   MONGODB_URI = process.env.MONGODB_URI;
 }
 
-const apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes window
-  max: 100, // INCREASE FOR MORE REQUESTS IF GETTING 429
-  statusCode: 429,
-  message: {
-    error: "Too Many Requests",
-    message: "You have exceeded your 100 requests per 15-minute quota."
-  },
-  standardHeaders: true, // Return standard draft-7 RateLimit-* headers
-  legacyHeaders: false,  // Disable the X-RateLimit-* headers
-});
-
 const PORT = 3001;
 
 mongoose.connect(MONGODB_URI)
@@ -36,7 +24,6 @@ mongoose.connect(MONGODB_URI)
   app.get("/test", (req, res) => {
   res.send("API is working");
 });
-  app.use('/api/users', apiLimiter); // limits to 100 api requests every 15 minutes per ip address for logging in only- all other pages behind jwt token auth
   app.use('/api/users', userRoutes); // use the route http://localhost:3000/api/users/createaccount or http://localhost:3000/api/users/login
   app.use('/api/game', gameRoutes); // use the routes from game.model.js with http://localhost:3000/api/game/routesingame.model.js
 //app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
